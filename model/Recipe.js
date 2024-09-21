b0b1e709f6868be77c18d942fc348ef69a628168
Astha Choudhury <123101231+AsthaChoudhury@users.noreply.github.com>
2024-09-21 15:21:31 +0530

    Update recipes.js to Recipe.js

---
+++ model/Recipe.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [String],
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  prepTime: {
+    type: String,
+    required: true,
+  },
+  cookTime: {
+    type: String,
+    required: true,
+  },
+  servings: {
+    type: Number,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+recipeSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+export default Recipe;
